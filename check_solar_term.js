2432710c0583ef3fe234353c0039c5ec28b87f6c
khanhnv <khanhnv@bap.jp>
2026-01-28 10:01:47 +0700

    Updates

--- check_solar_term.js
+++
@@ -1,7 +0,0 @@
-import { LunarCalendar } from '@dqcai/vn-lunar';
-
-const date = new Date(2024, 2, 20); // Around Vernal Equinox
-const lunar = LunarCalendar.fromSolar(date.getDate(), date.getMonth() + 1, date.getFullYear());
-
-console.log('Keys:', Object.keys(lunar));
-console.log('Lunar Object:', JSON.stringify(lunar, null, 2));
--- utils/index.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Utils index - re-export all utilities
- */
-
-export * from './moonPhase';
-export * from './hoangDao';
-export * from './dailyAdvice';
-export * from './calendarHelpers';
